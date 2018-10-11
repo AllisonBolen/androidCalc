a0abfaaa1242602f87a85ddba9c138e887a099cf
Allison Bolen <allisonbolen@Allisons-MBP.attlocal.net>
2018-10-11 15:26:18 -0400

    main

--- app/src/main/java/com/example/allisonbolen/calculatorandroid/MainActivity.java
+++ app/src/main/java/com/example/allisonbolen/calculatorandroid/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Button;
+import com.example.allisonbolen.calculatorandroid.UnitsConverter.VolumeUnits;
+import com.example.allisonbolen.calculatorandroid.UnitsConverter.LengthUnits;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,18 +15,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //internal vars
+        final boolean modeVal = False; // false = length | true = volume
+
+        // ui vars
         EditText fromTextBox = findViewById(R.id.fromEditText);
-        EditText toTextBox = findViewById(R.id.toEditText);
+        final EditText toTextBox = findViewById(R.id.toEditText);
         TextView fromView = findViewById(R.id.fromUnitTextView);
         TextView toView = findViewById(R.id.toUnitTextView);
         Button calc = findViewById(R.id.button4);
         Button clear = findViewById(R.id.button5);
         Button mode = findViewById(R.id.button3);
 
-        calc.setOnClickListener(v -> {
-            int from = Integer.parseInt(fromTextBox.getText().toString());
-            int to = Integer.parseInt(toTextBox.getText().toString());
+        mode.setOnClickListener(v -> {
+            modeVal = !modeVal;
+        });
 
+        calc.setOnClickListener(v -> {
+            int fromVal = Integer.parseInt(fromTextBox.getText().toString());
+            int toVal = Integer.parseInt(toTextBox.getText().toString());
+            UnitsConverter.VolumeUnits
+
+            // enum conversion
+
+            VolumeUnits from = VolumeUnits.valueOf();
+
+            if(modeVal){
+                //volume conversion
+                double lenVal = UnitsConverter.convert(VolumeUnits.Gallons,VolumeUnits.Liters,10.0)
+                toTextBox.setText(lenVal.toString());
+            }
         });
 
     }
